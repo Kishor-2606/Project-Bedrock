803efda3358eba69e7892780eac23623490a1207
Kishor-2606 <kishorkumarss2612@gmail.com>
2026-09-06 23:03:27 +0530

    Week 7 ml algorithms implementation

---
+++ week_7_ml_algorithms/unsupervised_learning/logistic_regression.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+data = pd.read_csv("customer_churn.csv")
+
+X = data[["Age","MonthlySpend"]]
+y = data["Churn"]
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y,test_size=0.2,random_state=42
+)
+
+model = LogisticRegression()
+model.fit(X_train,y_train)
+
+prediction = model.predict(X_test)
+
+print("Actual:",list(y_test))
+print("Predicted:",prediction)
